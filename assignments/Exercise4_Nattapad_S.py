b056e7023bbee98ed1968db5e7d03594ff3d34ef
Carrot_1358 <carrot1358@gmail.com>
2023-04-22 12:01:23 +0700

    finish Exercise4

--- assignments/Exercise4_Nattapad_S.py
+++ assignments/Exercise4_Nattapad_S.py
@@ -1,2 +1,34 @@
+"""
+"หากผู้เรียนกำลังศึกษาอยู่ในชั้นปีที่ 2 มหาวิทยาลัยชื่อดังย่านกลางเมืองที่ตอนนี้กำลังสอบเก็บคะแนนกันอย่างสนุกสนาน แล้ววันนึงได้รับมอบหมายจากอาจารย์ที่ปรึกษาให้พัฒนาโปรแกรมให้กับทางคณะ โดย ระบบดังกล่าวจะมีการเก็บคะแนนรายบุคคลของผู้เรียนแต่ละท่าน ของนักศึกษาชั้นปีที่ 2 เทอม 1" โดยมีรายชื่อวิชาดังนี้
 
-print("Hi")
+Foundation English
+General Business
+Introduction to Computer Systems
+Computer Programming
+
+
+โดยให้ผู้เรียนพัฒนาโปรแกรมโดยสร้างตัวแปรสำหรับเก็บคะแนนผู้เรียนในรายวิชาดังกล่าว โดยคะแนนจะสามารถเป็นตัวเลขทศนิยมได้ และ เมื่อได้ตัวแปรมาแล้วให้ทำการแสดงผลออกมาให้ผู้ใช้งานได้เห็นในรูปแบบ
+
+--- Your Score ---
+
+Foundation English : คะแนนที่ได้
+
+General Business : คะแนนที่ได้
+
+Introduction to Computer Systems : คะแนนที่ได้
+
+Computer Programming : คะแนนที่ได้
+
+*การตั้งชื่อตัวแปรควรสื่อความหมาย ผู้อ่าน หรือ นักพัฒนาโปรแกรมท่านอื่นสามารถอ่านเข้าใจได้
+"""
+
+Score_Foundation_English = 51.5
+Score_General_Business = 80.2
+Score_Introduction_to_Computer_Systems = 23.1
+Score_Computer_Programming = 25.6
+
+print("--- Your Score ---")
+print("Foundation English :", Score_Foundation_English)
+print("General Business :", Score_General_Business)
+print("Introduction to Computer Systems :", Score_Introduction_to_Computer_Systems)
+print("Computer Programming :", Score_Computer_Programming)
