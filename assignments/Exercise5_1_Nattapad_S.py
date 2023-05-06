b61f51fff78997fac2cea3e97cb2445117b72efc
Carrot_1358 <carrot1358@gmail.com>
2023-05-06 16:16:49 +0700

    exercise

---
+++ assignments/Exercise5_1_Nattapad_S.py
@@ -0,0 +1,6 @@
+A = 20
+B = 4
+print(A,"+",B,"=",A+B)
+print(A,"-",B,"=",A-B)
+print(A,"*",B,"=",A*B)
+print(A,"/",B,"=",int(A/B))
---
+++ assignments/Exercise5_2_Nattapad_S.py
@@ -0,0 +1,3 @@
+Speed = int(input("Distance(km): "))
+Time = int(input("Time(hr): "))
+print(f"{Speed / Time} km/h")
