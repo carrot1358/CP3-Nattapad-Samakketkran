4ff26de12044ec8f108d1f67dfc259b8c3d15562
Carrot_1358 <46210764+carrot1358@users.noreply.github.com>
2023-06-13 17:09:42 +0700

    update

---
+++ Lecture/Section 8/basic funcrion.py
@@ -0,0 +1,4 @@
+def sayhello():
+    print("Hello World")
+    print("Nattapad Samakketkran")
+sayhello()
---
+++ assignments/Lecture 50.py
@@ -0,0 +1,12 @@
+def Addnumber(x,y):
+    print(x+y)
+def Minusnumber(x,y):
+    print(x-y)
+def Multiplenum(x,y):
+    print(x*y)
+def Dividenum(x,y):
+    print(x/y)
+Addnumber(10,5)
+Minusnumber(10,5)
+Multiplenum(10,5)
+Dividenum(10,5)
